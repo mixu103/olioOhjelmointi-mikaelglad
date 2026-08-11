a38248748f3bb16b9d98b9134f6585f38e11abd2
Mikael Glad <mikael.glad@kukkula.local>
2026-08-11 14:27:50 +0300

    laskin valmis

--- public/build/calculator.js
+++ public/build/calculator.js
@@ -1,20 +1,33 @@
-"use strict";
-const inputA = document.getElementById("a");
-const inputB = document.getElementById("b");
-document.getElementById("+").addEventListener("click", () => {
-    const result = Number(inputA.value) + Number(inputB.value);
+function sum(a, b) {
+    return a + b;
+}
+function difference(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function division(a, b) {
+    return a / b;
+}
+function modulo(a, b) {
+    return (a % b);
+}
+function calculate(calculator) {
+    const inputA = document.getElementById("a");
+    const inputB = document.getElementById("b");
+    const result = calculator(Number(inputA.value), Number(inputB.value));
     document.getElementById("result").innerHTML = result.toString();
-});
-document.getElementById("-").addEventListener("click", () => {
-    const result = Number(inputA.value) - Number(inputB.value);
-    document.getElementById("result").innerHTML = result.toString();
-});
-document.getElementById("*").addEventListener("click", () => {
-    const result = Number(inputA.value) * Number(inputB.value);
-    document.getElementById("result").innerHTML = result.toString();
-});
-document.getElementById("/").addEventListener("click", () => {
-    const result = Number(inputA.value) / Number(inputB.value);
-    document.getElementById("result").innerHTML = result.toString();
-});
+}
+function registerCalculator(id, calculator) {
+    document.getElementById(id).addEventListener("click", (e) => {
+        calculate(calculator);
+    });
+}
+registerCalculator("+", sum);
+registerCalculator("-", difference);
+registerCalculator("*", multiply);
+registerCalculator("/", division);
+registerCalculator("%", modulo);
+export {};
 //# sourceMappingURL=calculator.js.map
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,30 +1,45 @@
-const inputA = document.getElementById("a") as HTMLInputElement;
-const inputB = document.getElementById("b") as HTMLInputElement;
 
-document.getElementById("+")!.addEventListener("click", () => {
-    const result =
-        Number(inputA.value) + Number(inputB.value);
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function difference(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function division(a: number, b: number): number {
+    return a / b;
+}
+
+function modulo(a: number, b: number): number {
+    return (a % b);
+}
+
+
+function calculate(calculator: (arg0: number, arg1: number) => number) {
+    const inputA: HTMLInputElement = document.getElementById("a") as HTMLInputElement;
+    const inputB: HTMLInputElement = document.getElementById("b") as HTMLInputElement;
+    
+    const result: number = calculator(Number(inputA.value), Number(inputB.value));
 
     document.getElementById("result")!.innerHTML = result.toString();
-});
+}
 
-document.getElementById("-")!.addEventListener("click", () => {
-    const result =
-        Number(inputA.value) - Number(inputB.value);
+function registerCalculator(id: string, calculator: (arg0: number, arg1: number) => number) {
+document.getElementById(id)!.addEventListener("click", (e:PointerEvent) => {
+        calculate(calculator);
+    })
+}
 
-    document.getElementById("result")!.innerHTML = result.toString();
-});
+registerCalculator("+", sum);
+registerCalculator("-", difference);
+registerCalculator("*", multiply);
+registerCalculator("/", division);
+registerCalculator("%", modulo);
 
-document.getElementById("*")!.addEventListener("click", () => {
-    const result =
-        Number(inputA.value) * Number(inputB.value);
 
-    document.getElementById("result")!.innerHTML = result.toString();
-});
-
-document.getElementById("/")!.addEventListener("click", () => {
-    const result =
-        Number(inputA.value) / Number(inputB.value);
-
-    document.getElementById("result")!.innerHTML = result.toString();
-});
+export {}
