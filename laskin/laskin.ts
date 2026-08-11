ec449b15fbd6e20a6e6d54891cff9e550da1938c
Mikael Glad <mikael.glad@kukkula.local>
2026-08-11 11:22:06 +0300

    laskin aloitetaan

---
+++ laskin/laskin.ts
@@ -0,0 +1,18 @@
+const number1 = document.getElementById('number1') as HTMLInputElement;
+const number2 = document.getElementById('number2') as HTMLInputElement;
+
+const AddBtn = document.getElementById("Add") as HTMLButtonElement;
+const SubBtn = document.getElementById("Sub") as HTMLButtonElement;
+const MultBtn = document.getElementById("Mult") as HTMLButtonElement;
+const DivBtn = document.getElementById("Div") as HTMLButtonElement;
+
+const printResult = document.getElementById("result") as HTMLOutputElement;
+
+function AddNumbers(){
+    let a = parseFloat(number1.value);
+    let b = parseFloat(number2.value);
+    let result = a + b;
+    printResult.textContent = result.toString();
+    
+}
+AddBtn.addEventListener("click", AddNumbers);
