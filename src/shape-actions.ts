4767b349eeb21cb20ab7fe90388a2fb72f1e6249
Mikael Glad <mikael.glad@kukkula.local>
2026-08-25 10:48:10 +0300

    kesken 3.1

---
+++ src/shape-actions.ts
@@ -0,0 +1,96 @@
+/**
+ * Defines the action protocol used by various components.
+ */
+export interface CanvasAction {
+
+    /**
+     * The name of the action
+     */
+    readonly name: string
+
+    readonly id: string
+
+    readonly status: string
+
+    /**
+     * This method is invoked by the controller. Controller is first level listener
+     * 
+     * @param e e browser mouse event
+     */
+    onClick(e: MouseEvent): void
+
+}
+
+
+abstract class BaseAction implements CanvasAction {
+
+    private _shapeView: ShapeViewer
+
+    public constructor(shapeView: ShapeViewer) {
+        this._shapeView = shapeView
+    }
+
+    public get shapeViewer(): ShapeViewer {
+        return this._shapeView
+    }
+
+    public get id(): string {
+        return this.name.toLowerCase().split(' ').join('-')
+    }
+    
+    public abstract get name(): string
+
+    public abstract get status(): string
+
+    public abstract onClick(e: MouseEvent): void
+
+}
+
+
+
+export class SelectAction extends BaseAction {
+
+    public get name(): string {
+        return "Select"
+    }
+
+    public get status(): string {
+        return "Click an shape to select it."
+    }
+
+    public onClick(e: MouseEvent): void {
+        // TODO: Implement this 
+        // throw new Error("Implement this method later")
+    }
+
+}
+
+
+
+//*
+export class AddShapeAction extends BaseAction {
+
+    private _shapeClass: any
+
+    public constructor(shapeClass: any, shapeViewer) {
+        super(shapeViewer)
+
+        this._shapeClass = shapeClass
+    }
+
+    public get name() {
+        return this._shapeClass.name
+    }
+
+    public get status() {
+        return 'Click to add a ${this.name}'
+    }
+
+    public onClick(e: MouseEvent): void {
+        // TODO: Implement this 
+        // throw new Error("Implement this method later")
+    }
+
+
+}
+//*/
