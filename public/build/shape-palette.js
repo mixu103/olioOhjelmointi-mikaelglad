3d01946304a4d13a0d6f5be02c2312628b3edd34
Mikael Glad <mikael.glad@kukkula.local>
2026-08-25 13:56:35 +0300

    tehty 3.2

--- public/build/shape-palette.js
+++ public/build/shape-palette.js
@@ -9,10 +9,39 @@ export class SelectedActionChangedEvent {
 export class PaletteComponent {
     constructor(paletteElement, actions) {
         this._listeners = [];
+        this._listeners = [];
+        this._selectedAction = actions[0];
         paletteElement.classList.add("v-box");
+        let html = "";
         actions.forEach(action => {
-            paletteElement.innerHTML += `<button id="${action.id}">${action.name}</button>`;
+            const button = document.createElement("button");
+            button.innerHTML = action.name;
+            button.addEventListener("click", e => {
+                console.log("Click on", action.name);
+                this._selectedAction = action;
+            });
+            paletteElement.appendChild(button);
         });
+        paletteElement.innerHTML = html;
+        actions.forEach(action => {
+            var _a;
+            (_a = document.getElementById(action.id)) === null || _a === void 0 ? void 0 : _a.addEventListener("click", e => {
+                console.log("Click on ", action.name);
+            });
+        });
+    }
+    get selectedAction() {
+        return this._selectedAction;
+    }
+    addPaletteListener(_listener) {
+        this._listeners.push(_listener);
+    }
+    fireSelectedActionChangedEvent(e) {
+        this._listeners.forEach(l => l.selectedActionChanged(e));
+    }
+    set selectedAction(selectedAction) {
+        this._selectedAction = selectedAction;
+        this.fireSelectedActionChangedEvent(new SelectedActionChangedEvent(selectedAction));
     }
 }
 //# sourceMappingURL=shape-palette.js.map
--- src/shape-palette.ts
+++ src/shape-palette.ts
@@ -27,13 +27,55 @@ export class PaletteComponent {
     private _selectedAction!: CanvasAction
 
     constructor(paletteElement: HTMLElement, actions: CanvasAction[]){
+
+
+        this._listeners = []
+        this._selectedAction = actions[0]
+
         paletteElement.classList.add("v-box")
 
+        let html = ""
+
         actions.forEach(action => {
-            paletteElement.innerHTML += `<button id="${action.id}">${action.name}</button>`
+            const button = document.createElement("button")
+            button.innerHTML = action.name
+            button.addEventListener("click", e => {
+                console.log("Click on", action.name)
+                this._selectedAction = action
+                
+            })
+            paletteElement.appendChild(button)
         })
+
+        paletteElement.innerHTML = html 
+
+        actions.forEach(action => {
+            document.getElementById(action.id)?.addEventListener("click", e => {
+                console.log("Click on ", action.name)
+            })
+        })
+
     }
 
-    
+
+    public get selectedAction(): CanvasAction {
+        return this._selectedAction
+    }
+
+    public addPaletteListener(_listener: PaletteListener) {
+        this._listeners.push(_listener)
+    }
+
+
+    private fireSelectedActionChangedEvent(e: SelectedActionChangedEvent) {
+        this._listeners.forEach(l => l.selectedActionChanged(e))
+    }
+
+    private set selectedAction(selectedAction: CanvasAction)  {
+        this._selectedAction = selectedAction
+        this.fireSelectedActionChangedEvent(new SelectedActionChangedEvent(selectedAction))
+
+    }
+
 
 }
