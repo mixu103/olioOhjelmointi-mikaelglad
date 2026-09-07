0a2fb6d12b116bc154e477f4ba3b880c4b199a2f
Mikael Glad <mikael.glad@kukkula.local>
2026-09-07 10:37:09 +0300

    tehty 3.8 osa 3 kohtaan 12:26

--- src/shape-properties.ts
+++ src/shape-properties.ts
@@ -3,12 +3,41 @@ import { ShapeSelectionEvent, ShapeSelectionListener } from "./shape-viewer.js"
 
 export class PropertiesComponent implements ShapeSelectionListener {
 
+    private _shape: Shape
+
     public constructor(propertiesElement: HTMLElement) {
 
+        propertiesElement.classList.add("v-box")
+
+        const label = document.createElement("label")
+        label.innerHTML = "Color"
+
+
+        const input: HTMLInputElement = document.createElement("input")
+        
+        const apply: HTMLButtonElement = document.createElement("button")
+        apply.innerHTML = "Apply"
+        apply.addEventListener("click", _ => {
+
+        })
+
+        propertiesElement.appendChild(label)
+        propertiesElement.appendChild(input)
+        propertiesElement.appendChild(apply)
+
+        input.disabled = true
+        apply.disabled = true
+    }
+
+    private setenabled(enabled: boolean): void {
+    
     }
 
     public shapeSelected(e: ShapeSelectionEvent): void {
         console.log("Shape selected in properties:", e.shape)
+        this._shape = e.shape
+
+
     }
 
     
